5c27a466e0b86a11e253506e37b31c6eea124055
Daniel Palma Garcia <danpalma94@gmail.com>
2026-03-12 19:11:05 -0600

    added jwt tokens generation

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt'
 import { UsersModule } from '../users/users.module';
+import { JWT_SECRET } from '../configs/jwt-secret';
+
 
 @Module({
   providers: [AuthService],
   controllers: [AuthController],
-  imports: [UsersModule]
+  imports: [
+    UsersModule,
+    JwtModule.register({
+      global: true,
+      secret: JWT_SECRET,
+      signOptions: { expiresIn: '1d'}, //[TODO]: Once proper secret var manager is implemented;
+                                       // make this an option that can be configured
+    }),
+  ],
 })
 export class AuthModule {}
