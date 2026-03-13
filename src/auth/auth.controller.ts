669752935750c9c8bf5b7d681dddffb62bd3003d
Daniel Palma Garcia <danpalma94@gmail.com>
2026-03-12 23:11:30 -0600

    added guard to a "/me" endpoint for testing purpouses

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, HttpCode, HttpStatus, NotImplementedException, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, NotImplementedException, Request, Post, UseGuards } from '@nestjs/common';
 
 import { AuthService } from './auth.service';
+import { AuthGuard } from './guards/auth.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -10,5 +11,11 @@ export class AuthController {
     @Post('login')
     login(@Body() input: { username: string; password: string}) {
         return this.authService.authenticate(input);
+    }
+
+    @UseGuards(AuthGuard)
+    @Get('me')
+    getUserInfo(@Request() request) {
+        return request.user
     }
 }
