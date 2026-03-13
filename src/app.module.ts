2284e9105e75ae432dbaedddc9324a10014a4ed4
Ismael Pérez <ipo778@hotmail.com>
2026-03-13 14:30:34 -0600

    feature: environment handler added, testing module added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TestModule } from './modules/test/test.module';
+import { ConfigModule } from '@nestjs/config';
+import { validateEnv } from './functions/environment-validator';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env.development',
+      validate: validateEnv
+    }),
+    TestModule
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService]
 })
-export class AppModule {}
+export class AppModule {
+}
---
+++ src/modules/test/test.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import * as nodemailer from 'nodemailer';
+
+@Injectable()
+export class TestService {
+  private transporter: any;
+
+  constructor(
+    private configService: ConfigService
+  ) {
+    this.transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: this.configService.get<string>('GMAIL_EMAIL'),
+        pass: this.configService.get<string>('GMAIL_APP_PASSWORD')
+      }
+    });
+  }
+
+  async test() {
+    this.configService.get;
+    const opciones = {
+      from: 'crabalgabas@gmail.com',
+      to: 'ismael.po@outlook.com',
+      subject: 'Recuperación de contraseña',
+      html: `<h1>Recupera tu cuenta</h1><p>Usa este token: ${'12365468sdf435sd21f32s1df'}</p>`
+    };
+
+    console.log('Sending email');
+    try {
+      await this.transporter.sendMail(opciones);
+      console.log('Email sent');
+      return true;
+    } catch (error) {
+      console.error(error);
+      return false;
+    }
+  }
+}
