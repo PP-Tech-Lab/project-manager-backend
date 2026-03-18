05a18ee8a37abdd1f131702878899c8c05f37de9
Daniel Palma Garcia <danpalma94@gmail.com>
2026-03-18 12:00:39 -0600

    removed previous jwt config

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -3,7 +3,6 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt'
 import { UsersModule } from '../users/users.module';
-import { JWT_SECRET } from '../../configs/jwt-secret';
 import { ConfigService, ConfigModule } from '@nestjs/config';
 
 @Module({
