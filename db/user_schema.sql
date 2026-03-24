e704538c49a485eca8c4723325683d9f37f1a996
Daniel Palma Garcia <danpalma94@gmail.com>
2026-03-23 23:56:20 -0600

    chore: added user tamble schema and removed comments

---
+++ db/user_schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS user_entity (
+userId integer PRIMARY KEY,
+username VARCHAR(20),
+password VARCHAR(20),
+email VARCHAR(35),
+isActivated BOOLEAN
+);
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -9,20 +9,6 @@ export type User = {
     password: string;
 }
 
-// [TODO] This is a mockup, implement a real ORM connection to DB
-// const users: User[] = [
-//     {
-//         userId: 1,
-//         username: 'Alice',
-//         password: 'topsecret', //TODO Use a hash
-//     },
-//     {
-//         userId: 2,
-//         username: 'Bob',
-//         password: '123abc'
-//     }
-// ];
-
 @Injectable()
 export class UsersService {
     constructor(
